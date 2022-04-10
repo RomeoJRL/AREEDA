4db3662af425ba980062d269c51c2b4cc43b2af1
RomeoJRL <jeanremy.lamah@usmba.ac.ma>
2022-04-10 00:30:20 +0000

    comment

---
+++ index.php
@@ -0,0 +1,473 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AREEDA HOME PAGE</title>
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    
+<!-- ------------------------------ -->
+
+<!-- ------------------------------ -->
+
+<header class="header">
+
+    <a href="#" class="logo"> <span>ARE</span>EDA </a>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+    <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#about">about</a>
+        <a href="#features">features</a>
+        <a href="#pricing">pricing</a>
+        <a href="#trainers">Our agents</a>
+        <a href="#blogs">Our team</a>
+        <a href="#blogs">Sign Up</a>
+    </nav>
+
+</header>
+
+<!-- ------------------------------ -->
+
+<!-- ------------------------------ -->
+<section class="home" id="home">
+
+    <div class="swiper home-slider">
+
+        <div class="swiper-wrapper">
+
+            <div class="swiper-slide slide" style="background: url(images/gp2.jpg) no-repeat;">
+                <div class="content">
+                    <span>be strong, be good</span>
+                    <h3>Never stop improving Your self</h3>
+                    <a  class="btn">get started</a>
+                </div>
+            </div>
+
+           
+
+        </div>
+
+        <div class="swiper-pagination"></div>
+
+    </div>
+
+</section>
+
+<!-- ------------------------------ -->
+
+<!-- ------------------------------ -->
+
+<section class="about" id="about">
+
+    <div class="image">
+        <img src="images/logo.jfif" alt="">
+    </div>
+
+    <div class="content">
+        <span>about us</span>
+        <h3 class="title">Every day is a chance to become better</h3>
+        <p>Vous vivez avec votre parent agé, vous travaillez vous cherchez quelqu'un de sûr pour s'occuper de lui ? Ne vous inquietez plus, vous etes à bon port,Nous vous offrons des services d'accompagnement de Qualité.</p>
+        <div class="box-container">
+            <div class="box">
+                <h3><i class="fas fa-check"></i>assistance à durée limitée</h3>
+                <p style="color: black;">Lorem ipsum .</p>
+            </div>
+            <div class="box">
+                <h3><i class="fas fa-check"></i>accompagnement quotidien</h3>
+                <p style="color: black;">Lorem ipsum .</p>
+            </div>
+            <div class="box">
+                <h3><i class="fas fa-check"></i>accompagnement pour des soins journaliers</h3>
+                <p style="color: black;">Lorem ipsum .</p>
+            </div>
+            <div class="box">
+                <h3><i class="fas fa-check"></i>assistance Permanente</h3>
+                <p style="color: black;">Lorem ipsum .</p>
+            </div>
+        </div>
+        <a  class="btn">read more</a>
+    </div>
+
+</section>
+<!-- ------------------------------ -->
+
+<!-- ------------------------------ -->
+
+<section class="features" id="features">
+
+    <h1 class="heading"> <span>Areeda features</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <div class="image">
+                <img src="images/sicom.jpeg" alt="">
+            </div>
+            <div class="content">
+                
+                <h3>La direction AREEDA</h3>
+                <p>consciente de l'enjeu actuel des choses ne recrute que des agents...</p>
+                <a  class="btn">read more</a>
+            </div>
+        </div>
+
+        <div class="box second">
+            <div class="image">
+                <img src="images/ass.jfif" alt="">
+            </div>
+            <div class="content">
+              
+                <h3>Social Worker</h3>
+                <p>Nos assistants assistants sociaux ,ont dabord suivis sont des seminaires de formation... </p>
+                <a  class="btn">read more</a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- ------------------------------ -->
+
+<!-- ------------------------------ -->
+
+<section class="pricing" id="pricing">
+
+    <div class="information">
+        <span>pricing plan</span>
+        <h3>affordable pricing plan for you</h3>
+        <a href="#pricing" class="btn">all details</a>
+    </div>
+
+    <div class="plan basic">
+        <h3>basic plan</h3>
+        <div class="price"><span>$</span>70<span>/mo</span></div>
+       <div class="list">
+        <p> <i class="fas fa-check"></i> assistance journaliere </p>
+        <p> <i class="fas fa-check"></i> 8h/j </p>
+      
+       </div>
+       <a  class="btn">get started</a>
+    </div>
+
+    <div class="plan">
+        <h3>premium plan</h3>
+        <div class="price"><span>$</span>100<span>/mo</span></div>
+       <div class="list">
+        <p> <i class="fas fa-check"></i> accompagnement à l'hopital </p>
+        <p> <i class="fas fa-check"></i> cuisine </p>
+        <p> <i class="fas fa-check"></i> Footing</p>
+        <p> <i class="fas fa-check"></i> assistance 6f/week</p>
+       </div>
+       <a  class="btn">get started</a>
+    </div>
+
+</section>
+
+<!-- ------------------------------ -->
+
+<!-- ------------------------------ -->
+
+<section class="trainers" id="trainers">
+
+    <h1 class="heading"> <span>Our Agents</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="images/a1.jfif" alt="">
+            <div class="content">
+                <span>social worker</span>
+                <h3>Elon Musk</h3>
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-pinterest"></a>
+                    <a href="#" class="fab fa-linkedin"></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/a1.jfif" alt="">
+            <div class="content">
+                <span>social worker</span>
+                <h3>Elon Musk</h3>
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-pinterest"></a>
+                    <a href="#" class="fab fa-linkedin"></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/a1.jfif" alt="">
+            <div class="content">
+                <span>social worker</span>
+                <h3>Elon Musk</h3>
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-pinterest"></a>
+                    <a href="#" class="fab fa-linkedin"></a>
+                </div>
+            </div>
+        </div>
+
+    
+
+    </div>
+
+</section>
+
+
+
+<section class="banner" id="ban">
+
+    <span>join us now</span>
+    <h3>Vous etes benevole</h3>
+    <p>en assistance sociale ,avec une experience d'au moins deux (2) ans ,ne tardez plus ,inscrivez et rejoignez le groupe AREEDA </p>
+    <a href="#ban" class="btn">sign Up</a>
+
+</section>
+
+
+<!-- ------------------------------ -->
+
+<!-- ------------------------------ -->
+
+
+
+<section class="review">
+
+    <div class="information">
+        <span>testimonials</span>
+        <h3>what our clients says</h3>
+        <p>Ci apres ,les feed-backs de nos Clients sur la qualité de nos services</p>
+        <a  class="btn">read more</a>
+    </div>
+
+    <div class="swiper review-slider">
+
+        <div class="swiper-wrapper">
+
+            <div class="swiper-slide slide">
+                <p>Je vis avec avec mon pere depuis quelques mois , vu son age j'etais obligé de m'absenter au service de temps à autre mais depuis que j'ai decouvert le groupe AREEDA, tout a changé mon pere se sent épanoui!...</p>
+                <div class="user">
+                    <img src="images/a1.jfif" alt="">
+                    <div class="info">
+                        <h3>Amine tarek</h3>
+                        <span>Telecoms Engineer</span>
+                    </div>
+                    <i class="fas fa-quote-left"></i>
+                </div>
+            </div>
+
+            <div class="swiper-slide slide">
+                <p>Je vis avec avec mon pere depuis quelques mois , vu son age j'etais obligé de m'absenter au service de temps à autre mais depuis que j'ai decouvert le groupe AREEDA, tout a changé mon pere se sent épanoui!...</p>
+                <div class="user">
+                    <img src="images/client.png" alt="">
+                    <div class="info">
+                        <h3>Hugues Koumako</h3>
+                        <span>software Engineer</span>
+                    </div>
+                    <i class="fas fa-quote-left"></i>
+                </div>
+            </div>
+
+            <div class="swiper-slide slide">
+                <p>Je vis avec avec mon pere depuis quelques mois , vu son age j'etais obligé de m'absenter au service de temps à autre mais depuis que j'ai decouvert le groupe AREEDA, tout a changé mon pere se sent épanoui!...</p>
+                <div class="user">
+                    <img src="images/client.png" alt="">
+                    <div class="info">
+                        <h3>Boro ousseni</h3>
+                        <span>embded systems Engineer</span>
+                    </div>
+                    <i class="fas fa-quote-left"></i>
+                </div>
+            </div>
+
+            <div class="swiper-slide slide">
+                <p>Je vis avec avec mon pere depuis quelques mois , vu son age j'etais obligé de m'absenter au service de temps à autre mais depuis que j'ai decouvert le groupe AREEDA, tout a changé mon pere se sent épanoui!...</p>
+                <div class="user">
+                    <img src="images/client.png" alt="">
+                    <div class="info">
+                        <h3>Karim temsani</h3>
+                        <span>designer</span>
+                    </div>
+                    <i class="fas fa-quote-left"></i>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+<!-- ------------------------------ -->
+
+<!-- ------------------------------ -->
+<section class="blogs" id="blogs">
+
+    <h1 class="heading"> <span>Our Team</span> </h1>
+
+    <div class="swiper blogs-slider">
+
+        <div class="swiper-wrapper">
+
+            <div class="swiper-slide slide">
+                <div class="image">
+                    <img src="images/bout.jpeg" alt="">
+                </div>
+                <div class="content">
+                    <div class="link"> <a href="#">SERIANI</a> <span>|</span> <a href="#">BOUTAYNA</a> </div>
+                    <h3>PDG of AREEDA</h3>
+                    <p>"together we can".</p>
+                    <a class="btn">Engineer Student</a>
+                </div>
+            </div>
+            
+            <div class="swiper-slide slide">
+                <div class="image">
+                    <img src="images/remi.png" alt="">
+                </div>
+                <div class="content">
+                    <div class="link"> <a href="#">LAMAH</a> <span>|</span> <a href="#">JEAN REMY</a> </div>
+                    <h3>Production Manager</h3>
+                    <p>"La vérité est la seule base solide à laquelle se fier".</p>
+                    <a class="btn">Engineer Student</a>
+                </div>
+            </div>
+
+            <div class="swiper-slide slide">
+                <div class="image">
+                    <img src="images/remi.png" alt="">
+                </div>
+                <div class="content">
+                    <div class="link"> <a href="#">Achraf</a> <span>|</span> <a href="#">Mzyene</a> </div>
+                    <h3>Communication Manager</h3>
+                    <p>"La vérité est la seule base solide à laquelle se fier".</p>
+                    <a class="btn">Engineer Student</a>
+                </div>
+            </div>
+
+            <div class="swiper-slide slide">
+                <div class="image">
+                    <img src="images/remi.png" alt="">
+                </div>
+                <div class="content">
+                    <div class="link"> <a href="#">Ez-zamrani</a> <span>|</span> <a href="#">Oumar</a> </div>
+                    <h3>Finance Manager</h3>
+                    <p>"La vérité est la seule base solide à laquelle se fier".</p>
+                    <a class="btn">Engineer Student</a>
+                </div>
+            </div>
+
+            <div class="swiper-slide slide">
+                <div class="image">
+                    <img src="images/a1.jfif" alt="">
+                </div>
+                <div class="content">
+                    <div class="link"> <a href="#">Fatiha</a> <span>|</span> <a href="#">El-manaoui</a> </div>
+                    <h3>Production Manager</h3>
+                    <p>"La vérité est la seule base solide à laquelle se fier".</p>
+                    <a class="btn">Engineer Student</a>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="swiper-pagination"></div>
+
+    </div>
+
+</section>
+
+
+
+<!-- ------------------------------ -->
+
+<!-- ------------------------------ -->
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a class="links" href="#home">home</a>
+            <a class="links" href="#about">about</a>
+            <a class="links" href="#features">features</a>
+            <a class="links" href="#pricing">pricing</a>
+            <a class="links" href="#trainers">Our agents</a>
+            <a class="links" href="#blogs">Our Team</a>
+            <a class="links" href="#blogs">Sign up</a>
+        </div>
+
+        
+        <div class="box">
+            <h3>Contacts</h3>
+            <p> <i class="fas fa-phone"></i> +212-6567890 </p>
+            <p> <i class="fas fa-phone"></i> +212-6227545 </p>
+            <p> <i class="fas fa-envelope"></i> sicom.2023@gmail.com </p>
+            <p> <i class="fas fa-map"></i> FEZ, MOROCCO - 3000 </p>
+            <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+                <a href="#" class="fab fa-pinterest"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <h3>newsletter</h3>
+            <p>message </p>
+            <form action="">
+                <textarea  name="" class="email" placeholder="Que pensez vous de nos services?" id=""></textarea>
+                <input type="submit" value="Ajouter" class="btn">
+            </form>
+        </div>
+
+    </div>
+
+</section>
+
+<div class="credit"> created by <span>REMY-JEAN</span> | Copyright &copy; 2022 all rights reserved! </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
